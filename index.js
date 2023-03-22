e7b49d3864dee87d451221a02c9fa5c6f3eff89f
makio0009 <andredobrawl7@gmail.com>
2023-03-22 18:19:06 -0300

    feat: add search terms and prefix

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+const readline = require('readline-sync')
+
 function start() {
     const content = {}
+
     content.searchTerm = askAndReturnTerm()
+    content.prefix = askAndReturnPrefix()
+
     function askAndReturnTerm() {
-        return 'TERM EXAMPLE'
+        return readline.question('Digite um termo de pesquisa no Wikipedia:')
+    }
+    function askAndReturnPrefix() {
+        const prefixes = ['O que é', 'Quem é', 'a história de', 'A história do']
+        const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Escolha uma opção:')
+        const selectedPrefixText = prefixes [selectedPrefixIndex]
+    
+    
+
+        return selectedPrefixText
     }
 
     console.log(content)
